b9e05d102a93d5f3e5b99769f528570a8513abfc
isanmartin <isanmartin@keedio.com>
2018-05-21 15:08:45 +0200

    Refactorized folder src to server

--- server/api.js
+++ server/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Joi = require('joi');
 const app = express();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
