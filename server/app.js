b94c748b1c86fc29f7ae7bc430bd95452f6a13e5
isanmartin <isanmartin@keedio.com>
2018-05-23 11:55:24 +0200

    Modified pipeline parameters

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "jade");
-app.set('views', __dirname);
+app.set('views', __dirname + '/views');
 
 app.get("/", function(req, res){
     res.render("index");
