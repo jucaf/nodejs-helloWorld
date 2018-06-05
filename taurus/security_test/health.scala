e7a9da3198d16f1fde053fea0a4320b9b7f03cff
isanmartin <isanmartin@keedio.com>
2018-06-05 11:23:59 +0200

    Add taurus elements

---
+++ taurus/security_test/health.scala
@@ -0,0 +1,37 @@
+package src.test.taurus.security_test
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class BasicSimulation extends Simulation {
+
+    // parse load profile from Taurus
+    val t_iterations = Integer.getInteger("iterations", 20).toInt
+    val t_concurrency = Integer.getInteger("concurrency", 5).toInt
+    val t_rampUp = Integer.getInteger("ramp-up", 1).toInt
+    val t_holdFor = Integer.getInteger("hold-for", 60).toInt
+    val t_throughput = Integer.getInteger("throughput", 100).toInt
+    val t_defaultAddress = System.getProperty("default-address", "https://digitalservicesx.evobanco.com")
+    val httpConf = http.baseURL(t_defaultAddress)
+
+
+
+
+    // 'forever' means each thread will execute scenario until
+    // duration limit is reached
+    val loopScenario = scenario("Loop Scenario").forever() {
+        exec(
+            http("health")
+                .get("/health")
+        )
+    }
+
+    val execution =
+        loopScenario
+            .inject(rampUsers(t_concurrency) over t_rampUp)
+            .protocols(httpConf)
+
+    setUp(execution).
+        throttle(jumpToRps(t_throughput), holdFor(t_holdFor)).
+        maxDuration(t_rampUp + t_holdFor)
+}
